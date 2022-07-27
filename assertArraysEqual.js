9ce35b5263051170e7da3c4a3bd77c09ca73ad93
Edlyn Shih <edlyn0418@gmail.com>
2022-07-26 23:28:05 -0400

    Implement assertArraysEqual function using eqArrays function for array comparison

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+const eqArrays = function (array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (!eqArrays(arr1, arr2)) {
+    console.log(`🚨🚨🚨 Assertion Failed: ${arr1} !== ${arr2}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
+assertArraysEqual(["1", "2", 3], ["1", "2", 3]) // => true
+assertArraysEqual(["1", "2", '8'], ["1", "2", 8]) // => false
+
