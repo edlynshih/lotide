5944f7c71aba2b40fb8d7fe53fd14a24065ab70a
Edlyn Shih <edlyn0418@gmail.com>
2022-08-03 17:40:04 -0400

    Refactor assertArraysEqual.js .

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,13 +1,4 @@
-const eqArrays = function (array1, array2) {
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-}
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function (arr1, arr2) {
   if (!eqArrays(arr1, arr2)) {
@@ -24,3 +15,4 @@ assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
 assertArraysEqual(["1", "2", 3], ["1", "2", 3]) // => true
 assertArraysEqual(["1", "2", '8'], ["1", "2", 8]) // => false
 
+module.exports = assertArraysEqual
