01ce63f3886fe1b4d9f20f238f1b3db45618c9b4
Edlyn Shih <edlyn0418@gmail.com>
2022-08-14 15:25:34 -0400

    Implement flatten function which will take in an array containing elements including nested arrays of elements, and return a flattened version of the array.

---
+++ flatten.js
@@ -0,0 +1,17 @@
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+
+const flatten = function(arr) {
+  if (!Array.isArray(arr)) {
+    return arr;
+  } else {
+    return arr.reduce((pre, cur) => Array.isArray(cur) ? pre.concat(flatten(cur)) : pre.concat(cur), [])
+  }
+}
+
+
+// console.log(flatten([[1, 2], [3, 4], 5, [6], 7])) // => [1, 2, 3, 4, 5, 6]
+// console.log(flatten([1, 2, [[3, 4]], 5, [6]]))
+// console.log(flatten([1, [2, [[3, 4]]], [[5]], [6], 7, 8]))
+// console.log(flatten([1, 2, 3, 4, 5, 6]))
+// console.log(flatten([1]))
