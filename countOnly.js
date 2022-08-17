0951f0bc7d0b49aa247697bc7460eb302a986901
Edlyn Shih <edlyn0418@gmail.com>
2022-08-17 15:44:21 -0400

    Refactor countOnly Function.

--- countOnly.js
+++ countOnly.js
@@ -1,17 +1,15 @@
 const assertEqual = require('./assertEqual');
 
-// allItems: an array of strings that we need to look through
-// itemsToCount: an object specifying what to count
-const countOnly = function (allItems, itemsToCount) {
+const countOnly = function(allItems, itemsToCount) {
   const results = {};
   for (const item of allItems) {
-    if (!itemsToCount[item]) { // itemsToCount[item] outputs the value of the key("Jason", Karima") in itemsToCount which is true, ture...etc
-      continue;
+    if(itemsToCount[item]) {
+      if(!results[item]) {
+        results[item] = 1;
+      } else {
+        results[item]++;
+      }
     }
-    if (!results[item]) {
-      results[item] = 0;
-    }
-    results[item]++;
   }
   return results;
 }
@@ -19,23 +17,22 @@ const countOnly = function (allItems, itemsToCount) {
 module.exports = countOnly;
 
 
-// const firstNames = [
-//   "Karl",
-//   "Salima",
-//   "Agouhanna",
-//   "Fang",
-//   "Kavith",
-//   "Jason",
-//   "Salima",
-//   "Fang",
-//   "Joe"
-// ];
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
 
-// console.log(countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false }));
 
-// const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
 
-// assertEqual(result1["Jason"], 1);
-// assertEqual(result1["Karima"], undefined);
-// assertEqual(result1["Fang"], 2);
-// assertEqual(result1["Agouhanna"], undefined);
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
