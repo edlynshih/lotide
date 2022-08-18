69a3b6a255ac3969095d12e453ef1b3c299b4952
Edlyn Shih <edlyn0418@gmail.com>
2022-08-17 21:45:31 -0400

    Refactor letterPositions function.

--- letterPositions.js
+++ letterPositions.js
@@ -4,14 +4,13 @@ const assertArraysEqual = require('./assertArraysEqual');
 const letterPositions = function(sentence) {
   const results = {};
   for (let i = 0; i < sentence.length; i++) {
-    let letter = sentence[i]; //i = index of letter
+    let letter = sentence[i]; 
     if (letter.toLowerCase() === letter.toUpperCase()) {
       continue;
     }
     if (!results[letter]) {
-      results[letter] = [];
-    } 
-    if (results[letter]) {
+      results[letter] = [i];
+    } else { 
       results[letter].push(i);
     }
   }
@@ -21,7 +20,7 @@ const letterPositions = function(sentence) {
 module.exports = letterPositions;
 
 
-// console.log(letterPositions('lighthouse in the house'));
+//console.log(letterPositions('lighthouse in the house'));
 
 // assertArraysEqual({h:[0], e: [1], l: [2,3], o: [4]}.h, [0]);
 // assertArraysEqual(letterPositions("hello").e, [1]);
