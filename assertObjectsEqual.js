416f680b0dbb422356d5be092b0eebf6661b3036
Edlyn Shih <edlyn0418@gmail.com>
2022-09-02 16:05:01 -0400

    defined the inspect function for assertObjectsEqual.

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -3,7 +3,7 @@ const eqObjects = require('./eqObjects');
 const assertObjectsEqual = function(obj1, obj2) {
   const inspect = require('util').inspect;
   if (!eqObjects(obj1, obj2)) {
-    console.log(`🚨🚨🚨 Assertion Failed: ${inspect(obj1)} !== ${inspect(obj2)}`);
+    console.log(`🚨🚨🚨 Assertion Failed: ${inspect(obj1)} !== ${inspect(obj2)}`); //using util library's inspect function to see detailed object output to the console.
   } else {
     console.log(`✅✅✅ Assertion Passed: ${inspect(obj1)} === ${inspect(obj2)}`);
   }
