c58c0dc6a7e7bfae1a41737c5dfe8b7fd9627121
Edlyn Shih <edlyn0418@gmail.com>
2022-09-02 12:21:55 -0400

    Refactor eqObjectsTest using mocha and chai.

--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -1,16 +1,27 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const eqObjects = require('../eqObjects');
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true); // => true
-
 const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false); // => false
-
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
-
 const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
+
+describe("#eqObjects", () => {
+  it("returns true if objects are equal", () => {
+    assert.deepEqual(eqObjects(ab, ba), true);
+  });
+
+  it("returns false if objects are not the same", () => {
+    assert.deepEqual(eqObjects(ab, abc), false);
+  });
+
+  it("returns true if objects are equal", () => {
+    assert.deepEqual(eqObjects(cd, dc), true);
+  });
+
+  it("returns false if objects are not the same", () => {
+    assert.deepEqual(eqObjects(cd, cd2), false);
+  });
+});
