8a3a7ae21328fe519d744329aa377ab00812078f
Edlyn Shih <edlyn0418@gmail.com>
2022-09-02 16:07:55 -0400

    added comments to the countLetters function.

--- countLetters.js
+++ countLetters.js
@@ -1,6 +1,6 @@
 const countLetters = function(sentence) {
   const results = {};
-  const string = sentence.toLowerCase().replace(/\s/g, "");
+  const string = sentence.toLowerCase().replace(/\s/g, ""); //convert the sentence to lower case and get rid of all space
   for (const letter of string) {
     if (!results[letter]) {
       results[letter] = 1;
