75687fe9f44e5f93759d881472ef2ea4d6b38ea1
Edlyn Shih <edlyn0418@gmail.com>
2022-09-01 22:02:46 -0400

    Refactor assertArraysEqual function and added a test file.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -8,12 +8,4 @@ const assertArraysEqual = function (arr1, arr2) {
   }
 };
 
-module.exports = assertArraysEqual
-
-
-// assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
-// assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
-// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
-// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
-// assertArraysEqual(["1", "2", 3], ["1", "2", 3]) // => true
-// assertArraysEqual(["1", "2", '8'], ["1", "2", 8]) // => false
+module.exports = assertArraysEqual;
